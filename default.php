3d6f5c9bf812fe65d2d6d50778130622e610453e
Francois Harbec <fharbec@gmail.com>
2010-03-29 12:24:33 -0700

    initial checkin

---
+++ default.php
@@ -0,0 +1,331 @@
+<?php 
+##########################################################################
+## @package Joomla 1.5
+## @copyright Copyright (C) 2005 Open Source Matters. All rights reserved.
+## @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+##
+## @component RD-Autos - Version 1.5.5
+## @copyright Copyright (C) Robert Dam - http://www.rd-media.org
+## @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+## Please do NOT remove this licence statement
+###########################################################################
+
+## Check if the file is included in the Joomla Framework
+defined('_JEXEC') or die ('No Acces to this file!');
+
+## Adding the AJAX part to the dropdowns & Lightbox functions
+$document =& JFactory::getDocument();
+$document->addScript( JURI::root(true).'/administrator/components/com_rdautos/helper/ajax.js');
+$document->addStyleSheet( 'components/com_rdautos/assets/horizontal-detail-view.css' );
+
+?>
+
+<script type="text/javascript">
+	
+var ajax = new Array();
+
+function getModelList(sel)
+{
+	var Code = sel.options[sel.selectedIndex].value;
+	document.getElementById('modelid').options.length = 0;	
+	if(Code.length>0){
+		var index = ajax.length;
+		ajax[index] = new sack();
+		ajax[index].requestFile = 'index.php?option=com_rdautos&controller=search&task=getModels&format=raw&makeid='+Code;	
+		ajax[index].onCompletion = function(){ createModels(index) };	
+		ajax[index].runAJAX();		// Execute AJAX function
+	}
+}
+
+function createModels(index)
+{
+	var obj = document.getElementById('modelid');
+	eval(ajax[index].response);	// Executing the response from Ajax as Javascript code	
+}
+
+</script>
+
+<script language="javascript">
+ 
+function getKeyCode(eventObject)
+
+{
+
+if (!eventObject) keyCode = window.event.keyCode; //IE
+
+else keyCode = eventObject.which; //Mozilla
+
+return keyCode;
+
+}
+
+function onlyNumeric(eventObject)
+
+{
+
+keyCode = getKeyCode(eventObject);
+
+if (((keyCode > 31) && (keyCode < 48)) || ((keyCode > 57) && (keyCode < 127)))
+
+{
+
+if (!eventObject) window.event.keyCode = 0; //IE
+
+else eventObject.preventDefault(); //Mozilla
+
+return false;
+
+}
+
+}
+
+</script>
+
+<style type="text/css">
+select{
+	width:140px;
+}
+</style>
+
+<h1 class="contentheading">
+<?php echo JText::_( 'SEARCH VEHICLE' ); ?> </h1>
+<?php echo JText::_( 'SEARCH EXPLANATION' ); ?><br><br>
+
+<?php $link = JRoute::_( 'index.php?option=com_rdautos&view=search&layout=results' ); ?>
+
+<div id="leaseinformation">
+
+<form action = "<?php echo $link; ?>" method="POST" name="Form" id="Form">
+<table width="100%" border="0" cellspacing="1" cellpadding="1">
+  <tr>
+    <td width="69%">
+        <div id="container">
+        	
+          	<div id="normal-search">			
+                
+            <table width="100%" border="0" cellspacing="1" cellpadding="1">
+              <tr> 
+                <td width="33%"><strong><?php echo JText::_( 'SEARCH MAKE' ); ?></strong></td>
+                <td width="33%"><strong><?php echo JText::_( 'SEARCH MODEL' ); ?></strong></td>
+                <td width="34%"><strong><?php echo JText::_( 'SEARCH CATEGORY' ); ?></strong></td>
+              </tr>
+              <tr> 
+                <td><?php echo $this->lists[make]; ?></td>
+                <td><?php echo $this->lists[model]; ?></td>
+                <td><?php echo $this->lists[catid]; ?></td>
+              </tr>
+             <!-- DAN REMOVED THE SECONDARY SEARCH FIELDS 
+	 			<tr> 
+                <td><strong><?php echo JText::_( 'SEARCH TRANS' ); ?></strong></td>
+                <td><strong><?php echo JText::_( 'SEARCH FUEL' ); ?></strong></td>
+                <td><strong><?php echo JText::_( 'SPEC DEALER' ); ?></strong></td>
+              </tr>
+              <tr> 
+                <td><?php echo $this->lists[transmission]; ?></td>
+                <td><?php echo $this->lists[fuel]; ?></td>
+                <td><?php echo $this->lists[dealer]; ?></td>
+              </tr>
+              <tr> 
+			-->
+			
+                <td><strong><?php echo JText::_( 'SEARCH YEAR' ); ?></strong></td>
+                <td><strong><!-- SEARCH STOCK # HEADER  HERE  <?php echo JText::_( 'Search Stock #' ); ?> --></strong></td> 
+                <td> <div align="left"><strong><?php echo JText::_( 'SEARCH PRICE' ); ?></strong> 
+                    <br>
+                  </div></td>
+              </tr>
+              <tr> 
+                <td height="24"><input name="buildFrom" type="text" class="inputbox" id="buildFrom" size="5" maxlength="4"
+                    onkeypress="onlyNumeric(arguments[0])" value="<?php echo $this->lists['yearfrom'];?>"/>
+-
+  <input name="buildTo" type="text" class="inputbox" id="buildTo" size="5" maxlength="4"
+                    onkeypress="onlyNumeric(arguments[0])" value="<?php echo $this->lists['yearto'];?>"/>                    </td>
+                
+
+
+
+
+<!-- STOCK NUMBER SEARCH HACK GOES HERE-->
+
+
+
+<td valign="top"> <!--
+	
+	<input name="mileagefrom" type="text" class="inputbox" id="mileagefrom" size="7" maxlength="7" 
+                value="<?php echo $this->lists['mileagefrom'];?>"/>
+
+  <input name="mileageTo" type="text" class="inputbox" id="mileageTo" size="5" maxlength="7"
+                  value="<?php echo $this->lists['mileageto'];?>" />
+
+--></td> 
+
+
+
+
+<!-- STOCK SEARCH HACK ENDS HERE -->
+
+
+
+
+
+                <td valign="top"> <div align="left">
+                    <input name="priceFrom" type="text" class="inputbox" id="priceFrom" size="5" maxlength="7"
+                    onkeypress="onlyNumeric(arguments[0])" value="<?php echo $this->lists['pricefrom'];?>" />
+                    - 
+                    <input name="priceTo" type="text" class="inputbox" id="priceTo" size="5" maxlength="7"
+                    onkeypress="onlyNumeric(arguments[0])" value="<?php echo $this->lists['priceto'];?>"/>
+                    <br>
+                  </div></td>
+              </tr>
+            </table>
+       	  </div>             
+        </div>
+    </td>
+  </tr>  
+</table>
+
+<!-- REMOVE ADITIONAL SEARCH OPTIONS  
+	
+	
+ <?php if ($this->config->show_optionlist != 0) { ?>	
+<h1 class="contentheading"><?php echo ' '. JText::_( 'EXTRA SEARCH OPTIONS' ); ?></h1>  
+
+<table width="100%" border="0">
+  <tr>
+	<td width="52%" align="left" valign="top"><table width="100%" border="0">
+	  <tr>
+		<td width="50%"><?php echo JText::_( 'AIRBAGS DRIVER' ); ?></td>
+		<td width="2%"><div align="center">:</div></td>
+		<td width="48%"><?php echo $this->lists[airbag_driver]; ?></td>
+	  </tr>
+	  <tr>
+		<td><?php echo JText::_( 'AIRBAGS PASSENGER' ); ?></td>
+		<td><div align="center">:</div></td>
+		<td><?php echo $this->lists[airbag_passengers]; ?></td>
+	  </tr>
+	  <tr>
+		<td><?php echo JText::_( 'AIRBAGS LATTERAL' ); ?></td>
+		<td><div align="center">:</div></td>
+		<td><?php echo $this->lists[airbag_latteral]; ?></td>
+	  </tr>
+	  <tr>
+		<td width="50%"><?php echo JText::_( 'AIRBAGS BACK' ); ?></td>
+		<td width="2%"><div align="center">:</div></td>
+		<td width="48%"><?php echo $this->lists[airbag_back]; ?></td>
+	  </tr>
+	  <tr>
+		<td><?php echo JText::_( 'WINDOW HEATERS' ); ?></td>
+		<td><div align="center">:</div></td>
+		<td><?php echo $this->lists[windowheaters]; ?></td>
+	  </tr>
+	  <tr>
+		<td><?php echo JText::_( 'SUNROOF' ); ?></td>
+		<td><div align="center">:</div></td>
+		<td><?php echo $this->lists[sunroof]; ?></td>
+	  </tr>
+	  <tr>
+		<td><?php echo JText::_( 'DIGI AIR CONDTIONER' ); ?></td>
+		<td><div align="center">:</div></td>
+		<td><?php echo $this->lists[digitalaircondition]; ?></td>
+	  </tr>
+	  <tr>
+		<td><?php echo JText::_( 'ELECTRIC WINDOWS' ); ?></td>
+		<td><div align="center">:</div></td>
+		<td><?php echo $this->lists[electricwindows]; ?></td>
+	  </tr>
+	  <tr>
+		<td><?php echo JText::_( 'ELECTRIC MIRRORS' ); ?></td>
+		<td><div align="center">:</div></td>
+		<td><?php echo $this->lists[electricmirrors]; ?></td>
+	  </tr>
+	  <tr>
+		<td><?php echo JText::_( 'ELECTRIC LOCKS' ); ?></td>
+		<td><div align="center">:</div></td>
+		<td><?php echo $this->lists[electriclocks]; ?></td>
+	  </tr>                  
+	  <tr>
+		<td><?php echo JText::_( 'HEATER' ); ?></td>
+		<td><div align="center">:</div></td>
+		<td><?php echo $this->lists[heather]; ?></td>
+	  </tr>
+			  
+	</table></td>
+	<td width="48%" align="left" valign="top"><table width="100%" border="0">
+	  <tr>
+		<td><?php echo JText::_( 'CD PLAYER' ); ?></td>
+		<td><div align="center">:</div></td>
+		<td><?php echo $this->lists[cdplayer]; ?></td>
+	  </tr>  
+	  <tr>
+		<td><?php echo JText::_( 'DVD PLAYER' ); ?></td>
+		<td><div align="center">:</div></td>
+		<td><?php echo $this->lists[dvdplayer]; ?></td>
+	  </tr>   
+	  <tr>
+		<td><?php echo JText::_( 'MP3 PLAYER' ); ?></td>
+		<td><div align="center">:</div></td>
+		<td><?php echo $this->lists[mp3player]; ?></td>
+	  </tr>                                          			  
+	  <tr>
+		<td><?php echo JText::_( 'IPOD COMPATIBLE' ); ?></td>
+		<td><div align="center">:</div></td>
+		<td><?php echo $this->lists[ipodcompatible]; ?></td>
+	  </tr>   
+	  <tr>
+		<td><?php echo JText::_( 'AMFM RADIO' ); ?></td>
+		<td><div align="center">:</div></td>
+		<td><?php echo $this->lists[amfmradio]; ?></td>
+	  </tr>     
+	  <tr>
+		<td><?php echo JText::_( 'HYDROLIC STEERING' ); ?></td>
+		<td><div align="center">:</div></td>
+		<td><?php echo $this->lists[hydraulicsteering]; ?></td>
+	  </tr>                           
+	  <tr>
+		<td><?php echo JText::_( 'CRUISE CONTROL' ); ?></td>
+		<td><div align="center">:</div></td>
+		<td><?php echo $this->lists[cruisecontrol]; ?></td>
+	  </tr>    
+	  <tr>
+		<td><?php echo JText::_( 'ABS BRAKES' ); ?></td>
+		<td><div align="center">:</div></td>
+		<td><?php echo $this->lists[absbrakes]; ?></td>
+	  </tr>   
+	  <tr>
+		<td><?php echo JText::_( 'FOG LIGHTS' ); ?></td>
+		<td><div align="center">:</div></td>
+		<td><?php echo $this->lists[foglights]; ?></td>
+	  </tr>
+	  <tr>
+		<td width="51%"><?php echo JText::_( 'LEATHER SEATS' ); ?></td>
+		<td width="4%"><div align="center">:</div></td>
+		<td width="45%"><?php echo $this->lists[leatherseats]; ?> </td>
+	  </tr> 
+	  <tr>
+		<td><?php echo JText::_( 'SEAT HEATER' ); ?></td>
+		<td><div align="center">:</div></td>
+		<td><?php echo $this->lists[seatsheater]; ?></td>
+	  </tr>                                  
+	</table></td>
+ </tr>
+</table>            
+
+<?php } ?>
+
+-->
+
+
+<br />
+<table width="100%" border="0">
+  <tr>
+    <td><input type="submit" name="button" id="button" class="button" 
+    value="<?php echo JText::_( 'PERFORM SEARCH NOW' ); ?>" /></td>
+  </tr>
+</table>
+
+</div>
+
+
+<br /><br />
+<input type="hidden" name="layout" value="results" />
+</form>
---
+++ search.php
@@ -0,0 +1,292 @@
+<?php
+##########################################################################
+## @package Joomla 1.5
+## @copyright Copyright (C) 2005 Open Source Matters. All rights reserved.
+## @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+##
+## @component RD-Autos - Version 1.5.5
+## @copyright Copyright (C) Robert Dam - http://www.rd-media.org
+## @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+## Please do NOT remove this licence statement
+###########################################################################
+
+defined('_JEXEC') or die();
+
+jimport('joomla.application.component.model');
+
+
+class RDAutosModelSearch extends JModel {
+
+	   var $_total = null;
+	   var $_pagination = null;
+	   var $_data = null;
+	   var $_ordering = null;
+
+   function __construct(){
+   
+      parent::__construct();
+		
+		global $mainframe;
+		
+		$config = JFactory::getConfig();
+		
+		// Get the pagination request variables
+		$limit        = $mainframe->getUserStateFromRequest( 'global.list.limit', 'limit', $mainframe->getCfg('list_limit'), 'int' );
+		$limitstart    = JRequest::getInt('limitstart', 0);
+		
+		// In case limit has been changed, adjust limitstart accordingly
+		$limitstart = ($limit != 0 ? (floor($limitstart / $limit) * $limit) : 0);
+		
+		$this->setState('limit', $limit);
+		$this->setState('limitstart', $limitstart);
+  
+   }
+
+   function getData()
+   {
+      if (empty($this->_data))
+      {
+         $db = JFactory::getDBO();
+
+		## Making the query for showing all the cars in list function
+		$sql = 'SELECT * FROM #__rdautos_config WHERE id = 1 ';
+		 
+         $db->setQuery($sql);
+         $this->data = $db->loadObject();
+      }
+      return $this->data;
+   }
+
+	function getPagination() {
+		
+		if (empty($this->_pagination)) {
+		
+			jimport('joomla.html.pagination');
+			$this->_pagination = new JPagination( $this->getTotal(), $this->getState('limitstart'), $this->getState('limit') );
+		}
+	
+		return $this->_pagination;
+	}
+    
+    function getTotal() {
+	
+        if (empty($this->_total)) {
+
+			$where		= $this->_buildContentWhere();
+			
+				$query = 'SELECT  a.*, b.*, c.*, d.* 
+						  FROM #__rdautos_information AS a, #__rdautos_models AS b, 
+						  #__rdautos_makes AS c, #__rdautos_categories as d'
+						.$where;
+					  
+            $this->_total = $this->_getListCount($query, $this->getState('limitstart'), $this->getState('limit'));
+        }
+
+        return $this->_total;
+    }  
+
+	function _buildContentWhere() {
+	
+		global $mainframe, $option;
+		
+		$db					=& JFactory::getDBO();
+		
+		## Pick filled in values first, we can use them later on if a user wants to change his values. (DROPDOWNS)
+		$makeid     		= $mainframe->getUserStateFromRequest( $option.'makeid', 'makeid', '0', 'cmd' );
+		$modelid     		= $mainframe->getUserStateFromRequest( $option.'modelid', 'modelid', '0', 'cmd' );
+		$catid	     		= $mainframe->getUserStateFromRequest( $option.'catid', 'catid', '0', 'cmd' );
+		$dealerid     		= $mainframe->getUserStateFromRequest( $option.'dealerid', 'dealerid', '0', 'cmd' );
+		$fueltype     		= $mainframe->getUserStateFromRequest( $option.'fueltype', 'fueltype', '0', 'cmd' );
+		$transmission		= $mainframe->getUserStateFromRequest( $option.'transmission', 'transmission', '0', 'cmd' );
+		
+		## This part will pick up all the filled in values.	(INPUTBOXES)
+		$mileagefrom		= $mainframe->getUserStateFromRequest( $option.'mileageFrom', 'mileageFrom', '0', 'string' );
+		$mileagefrom		= JString::strtolower( $mileagefrom );	
+		$mileageto			= $mainframe->getUserStateFromRequest( $option.'mileageTo', 'mileageTo', '0', 'string' );
+		$mileageto			= JString::strtolower( $mileageto );	
+		
+		$yearfrom			= $mainframe->getUserStateFromRequest( $option.'buildFrom', 'buildFrom', '0', 'string' );
+		$yearfrom			= JString::strtolower( $yearfrom );	
+		$yearto				= $mainframe->getUserStateFromRequest( $option.'buildTo', 'buildTo', '0', 'string' );
+		$yearto				= JString::strtolower( $yearto );	
+		
+		$pricefrom			= $mainframe->getUserStateFromRequest( $option.'priceFrom', 'priceFrom', '0', 'string' );
+		$pricefrom			= JString::strtolower( $pricefrom );	
+		$priceto			= $mainframe->getUserStateFromRequest( $option.'priceTo', 'priceTo', '1', 'string' );
+		$priceto			= JString::strtolower( $priceto );	
+		
+		$leatherseats = $mainframe->getUserStateFromRequest( $option.'leatherseats', 'leatherseats', '0', 'int' );
+		$airbag_driver = $mainframe->getUserStateFromRequest( $option.'airbag_driver', 'airbag_driver', '0', 'string' );
+		$airbag_passengers = $mainframe->getUserStateFromRequest( $option.'airbag_passengers', 'airbag_passengers', '0', 'string' );
+		$airbag_latteral = $mainframe->getUserStateFromRequest( $option.'airbag_latteral', 'airbag_latteral', '0', 'string' );
+		$airbag_back = $mainframe->getUserStateFromRequest( $option.'airbag_back', 'airbag_back', '0', 'string' );
+		$windowheaters = $mainframe->getUserStateFromRequest( $option.'windowheaters', 'windowheaters', '0', 'string' );
+		$sunroof = $mainframe->getUserStateFromRequest( $option.'sunroof', 'sunroof', '0', 'string' );
+		$digitalaircondition = $mainframe->getUserStateFromRequest( $option.'digitalaircondition','digitalaircondition','0','string');
+		$electricwindows = $mainframe->getUserStateFromRequest( $option.'electricwindows', 'electricwindows', '0', 'string' );
+		$electriclocks = $mainframe->getUserStateFromRequest( $option.'electriclocks', 'electriclocks', '0', 'string' );
+		$electricmirrors = $mainframe->getUserStateFromRequest( $option.'electricmirrors', 'electricmirrors', '0', 'string' );
+		$cdplayer = $mainframe->getUserStateFromRequest( $option.'cdplayer', 'cdplayer', '0', 'string' );
+		$dvdplayer = $mainframe->getUserStateFromRequest( $option.'dvdplayer', 'dvdplayer', '0', 'string' );
+		$mp3player = $mainframe->getUserStateFromRequest( $option.'mp3player', 'mp3player', '0', 'string' );
+		$ipodcompatible = $mainframe->getUserStateFromRequest( $option.'ipodcompatible', 'ipodcompatible', '0', 'string' );
+		$amfmradio = $mainframe->getUserStateFromRequest( $option.'amfmradio', 'amfmradio', '0', 'string' );
+		$cruisecontrol = $mainframe->getUserStateFromRequest( $option.'cruisecontrol', 'cruisecontrol', '0', 'string' );
+		$absbrakes = $mainframe->getUserStateFromRequest( $option.'absbrakes', 'absbrakes', '0', 'string' );
+		$heather = $mainframe->getUserStateFromRequest( $option.'heather', 'heather', '0', 'string' );
+		$seatsheater = $mainframe->getUserStateFromRequest( $option.'seatsheater', 'seatsheater', '0', 'string' );
+		$foglights = $mainframe->getUserStateFromRequest( $option.'foglights', 'foglights', '0', 'string' );
+		$hydraulicsteering = $mainframe->getUserStateFromRequest( $option.'hydraulicsteering', 'hydraulicsteering', '0', 'string' );		
+
+		$where = array();
+
+		$where[] = 'a.makeid = c.makeid';
+		$where[] = 'a.modelid = b.modelid';
+		$where[] = 'a.catid = d.catid';
+		$where[] = 'a.published = 1';
+		
+		if ($makeid != 0){ 
+
+            $where[] = 'a.makeid = '.(int)$makeid;
+		
+               if ($modelid > 0){ 
+                            $where[] = 'a.modelid = '.(int)$modelid;
+                }
+        }
+		
+		if ($catid != 0){ 
+			$where[] = 'a.catid = '.(int)$catid; 
+		}
+		if ($dealerid != 0){ 
+			$where[] = 'a.dealerid = '.(int)$dealerid;
+		}
+		if ($fueltype != 0){ 
+			$where[] = 'a.fueltype = '.(int)$fueltype;
+		}
+		if ($transmission != 0){ 
+			$where[] = 'a.transmission = '.(int)$transmission;
+		}
+		
+		## Now let's make a filter on the inputboxes	
+		if ($mileagefrom != 0){ 
+			$where[] = 'a.mileage > '.(int)$mileagefrom; 
+		}
+		if ($mileageto != 0){ 
+			$where[] = 'a.mileage < '.(int)$mileageto; 
+		}
+		
+		if ($pricefrom != 0){ 
+			$where[] = 'a.price > '.(int)$pricefrom; 
+		}
+		if ($priceto != 0){ 
+			$where[] = 'a.price < '.(int)$priceto; 
+		}		
+		
+		if ($yearfrom != 0 && $yearto == 0){ 
+			$where[] = 'a.constructed > '.(int)$yearfrom-1; 
+		}
+		if ($yearfrom == 0 && $yearto != 0){ 
+			$where[] = 'a.constructed < '.(int)$yearto+1;  
+		}		
+		if ($yearfrom != 0 && $yearto != 0)  {
+			## Create year + one to get till begin of the year
+			$year = $yearto+1;
+			$where[] = 'a.constructed > '.(int)$yearfrom; 
+			$where[] = 'a.constructed < '.(int)$year; 
+		}
+		
+		if ((int)$leatherseats != 0){
+			$where[] = 'a.leatherseats = 1';
+		}
+		if ((int)$airbag_driver != 0){
+			$where[] = 'a.airbag_driver = 1';
+		}
+		if ((int)$airbag_passengers != 0){
+			$where[] = 'a.airbag_passengers = 1';
+		}
+		if ((int)$airbag_latteral != 0){
+			$where[] = 'a.airbag_latteral = 1';
+		}			
+		if ((int)$airbag_back != 0){
+			$where[] = 'a.airbag_back = 1';
+		}
+		if ((int)$windowheaters != 0){
+			$where[] = 'a.windowheaters = 1';
+		}	
+		if ((int)$sunroof != 0){
+			$where[] = 'a.sunroof = 1';
+		}		
+		if ((int)$digitalaircondition != 0){
+			$where[] = 'a.digitalaircondition = 1';
+		}	
+		if ((int)$electricwindows != 0){
+			$where[] = 'a.electricwindows = 1';
+		}	
+		if ((int)$electriclocks != 0){
+			$where[] = 'a.electriclocks = 1';
+		}		
+		if ((int)$electricmirrors != 0){
+			$where[] = 'a.electricmirrors = 1';
+		}	
+		if ((int)$cdplayer != 0){
+			$where[] = 'a.cdplayer = 1';
+		}
+		if ((int)$dvdplayer != 0){
+			$where[] = 'a.dvdplayer = 1';
+		}	
+		if ((int)$mp3player != 0){
+			$where[] = 'a.mp3player = 1';
+		}	
+		if ((int)$ipodcompatible != 0){
+			$where[] = 'a.ipodcompatible = 1';
+		}
+		if ((int)$amfmradio != 0){
+			$where[] = 'a.amfmradio = 1';
+		}	
+		if ((int)$hydraulicsteering != 0){
+			$where[] = 'a.hydraulicsteering = 1';
+		}	
+		if ((int)$cruisecontrol != 0){
+			$where[] = 'a.cruisecontrol = 1';
+		}	
+		if ((int)$absbrakes != 0){
+			$where[] = 'a.absbrakes = 1';
+		}
+		if ((int)$heather != 0){
+			$where[] = 'a.heather = 1';
+		}	
+		if ((int)$seatsheater != 0){
+			$where[] = 'a.seatsheater = 1';
+		}	
+		if ((int)$foglights != 0){
+			$where[] = 'a.foglights = 1';
+		}																																										
+		
+		$where 		= ( count( $where ) ? ' WHERE '. implode( ' AND ', $where ) : '' );
+
+		return $where;
+	}
+
+
+   function getList() {
+   
+		if (empty($this->_data)) {
+
+		 	$db = JFactory::getDBO();
+			
+			$where		= $this->_buildContentWhere();
+						
+				## Making the query for showing all the cars in list function
+				$sql='SELECT  a.*, b.*, c.*, d.* 
+						  FROM #__rdautos_information AS a, #__rdautos_models AS b, 
+						  #__rdautos_makes AS c, #__rdautos_categories as d'
+						.$where; 
+			 
+				$db->setQuery($sql, $this->getState('limitstart'), $this->getState('limit' ));
+				$this->data = $db->loadObjectList();
+		}
+		return $this->data;
+	}
+	
+}
+?>
